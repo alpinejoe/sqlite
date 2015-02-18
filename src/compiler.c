04bd8b15545d548a73ed6cf1038b2513fc95bfa1
alpinejoe <paras.sethia+gh@gmail.com>
2015-02-18 21:41:47 +0530

    Limit SQL comparisons by checking against hash

--- src/compiler.c
+++ src/compiler.c
@@ -6,8 +6,65 @@
 #define FUNCTION_PROTOTYPE "SQLITE_PRIVATE int sqlite3ExecuteCompiledSql(Parse *pParse, const char *zSql, char **pzErrMsg)"
 #define DUMMY_SQL "SELECT * FROM sqlite_master"
 
+unsigned get_hash(char *zSql, size_t nChar){
+  unsigned hash=0;
+  for( size_t i=0;i<nChar;++i ){
+    hash^=zSql[i];
+    hash+=(hash<<1)+(hash<<4)+(hash<<7)+(hash<<8)+(hash<<24);
+  }
+  return hash;
+}
+
+void escape_sql(char *zSql, size_t nChar, char *zEscaped){
+  size_t j=0;
+  for( size_t i=0;i<nChar;++i ){
+    switch( zSql[i] ){
+      case '"':
+      case '\\':
+        zEscaped[j++]='\\';
+        zEscaped[j++]=zSql[i];
+        break;
+      case '\n':
+        zEscaped[j++]='\\';
+        zEscaped[j++]='n';
+        break;
+      case '\t':
+        zEscaped[j++]='\\';
+        zEscaped[j++]='t';
+        break;
+      default:
+        zEscaped[j++]=zSql[i];
+    }
+  }
+  zEscaped[j]='\0';
+}
+
+void compile_sql(sqlite3 *db,char *zSql){
+  sqlite3_stmt *pStmt=NULL;
+  size_t sqllen=strlen( zSql );
+  unsigned hash=get_hash( zSql,sqllen );
+
+  char *sqlescaped=malloc( sqllen*2 );
+  escape_sql( zSql,sqllen,sqlescaped );
+
+  printf( "    case %d: ",hash );
+  printf( "if( strcmp( zSql,\"%s\" )==0 ){\n",sqlescaped );
+  sqlite3_prepare_v2(db,zSql,sqllen,&pStmt,NULL);
+  if( SQLITE_OK!=sqlite3_errcode(db) ){
+    fprintf(stderr,"Error: unable to prepare SQL \"%s\": %s\n",
+      zSql,sqlite3_errmsg( db ));
+  }
+  else{
+    sqlite3_step( pStmt );
+  }
+  sqlite3_finalize( pStmt );
+  free( sqlescaped );
+  printf( "\n    }\n" );
+  printf( "    break;\n" );
+}
+
 /* Usage: compiler db sql ...*/
-int main( int argc, char **argv ){
+int main( int argc,char **argv ){
   if( argc < 2 ){
     fprintf(stderr, "Database path or SQL not provided. Appending empty function.\n");
     FILE *sqlite_source = freopen("sqlite3.c","a",stdout);
@@ -31,7 +88,7 @@ int main( int argc, char **argv ){
     }
 
     /* Dummy SQL to force SQLite initialisation */
-    freopen("nul","w",stdout); /* ignore if failed */
+    freopen("NUL","w",stdout); /* ignore if failed */
     sqlite3_prepare_v2(db,DUMMY_SQL,strlen( DUMMY_SQL ),&pStmt,NULL);
     sqlite3_finalize( pStmt );
 
@@ -41,18 +98,42 @@ int main( int argc, char **argv ){
       exit(1);
     }
 
-    printf (FUNCTION_PROTOTYPE " {\n  ");
-    for( int i=2; i<argc; ++i ){
-      printf( "if( strcmp(zSql, \"%s\")==0 ){\n",argv[i] );
-      sqlite3_prepare_v2(db,argv[i],strlen( argv[i] ),&pStmt,NULL);
-      if( SQLITE_OK!=sqlite3_errcode(db) ){
-        fprintf(stderr,"Error: unable to prepare SQL \"%s\": %s\n",
-          argv[i],sqlite3_errmsg( db ));
+    printf( FUNCTION_PROTOTYPE " {\n" );
+    printf( "  unsigned hash=0;\n" );
+    printf( "  size_t sqllen=strlen( zSql );\n" );
+    printf( "  for( size_t i=0; i<sqllen; ++i ){\n" );
+    printf( "    hash^=zSql[i];\n" );
+    printf( "    hash+=(hash<<1)+(hash<<4)+(hash<<7)+(hash<<8)+(hash<<24);\n" );
+    printf( "  }\n" );
+    printf( "  switch( hash ){\n" );
+    if( argc==2 ){
+      /* Read SQL from stdin. Queries are separated by an empty line. */
+      char buffer[1024];
+      char *sql=NULL;
+      int read;
+      int total=0;
+      while( fgets( buffer,sizeof( buffer ),stdin ) ){
+        read=strlen( buffer );
+        if( read==1 && total>0 ){
+          sql[total-1]='\0'; /* replace last '\n' */
+          compile_sql( db,sql );
+          total=0;
+        }
+        else{
+          sql=realloc( sql,total+read+1 );
+          memmove( sql+total,buffer,read+1 );
+          total+=read;
+        }
       }
-      sqlite3_finalize( pStmt );
-      printf( "\n  }\n  else " );
+      free( sql );
     }
-    printf( "return 0; /* SQL is not compiled */\n"
+    else{
+      for( int i=2; i<argc; ++i ){
+        compile_sql( db,argv[i] );
+      }
+    }
+    printf( "    default: return 0; /* SQL is not compiled */\n"
+            "  }\n"
             "  pParse->zTail=zSql+strlen( zSql );\n"
             "  return 1;\n"
             "}\n"
